5ad82bd23744085b264240c9202d668a585b07a5
Andy Burke <aburke@bitflood.org>
2014-06-10 07:00:43 -0700

    Make sure to remove tags from tag list so re-adding will work.

--- index.js
+++ index.js
@@ -397,6 +397,10 @@ Tagging.prototype.add_tag = function( $type_zone, text, actual_settings, skipEmi
                 $target : $tag,
                 $parent : _this.$el
             });
+            var existingIndex = _this.tags.indexOf( $tag );
+            if ( existingIndex > -1 ) {
+                _this.tags.splice( existingIndex, 1 );
+            }
             $tag.remove();
         })
         // finally append close button to tag element
